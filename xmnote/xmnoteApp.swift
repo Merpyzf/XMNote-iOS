8b48b316d890628aa869b73b1e59961a3fd8b436
Wangke <1052060838@qq.com>
2026-02-09 13:46:30 +0800

    Initial Commit

---
+++ xmnote/xmnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  xmnoteApp.swift
+//  xmnote
+//
+//  Created by 王珂 on 2026/2/9.
+//
+
+import SwiftUI
+
+@main
+struct xmnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xmnoteTests/xmnoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  xmnoteTests.swift
+//  xmnoteTests
+//
+//  Created by 王珂 on 2026/2/9.
+//
+
+import Testing
+@testable import xmnote
+
+struct xmnoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
